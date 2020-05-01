072283e7ade394898974c1050b86892666e46b78
Mark Wissler <mark.wissler@exaptive.com>
2020-05-01 15:30:20 -0500

    add heroku build steps to package.json

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Some site words here.
+          Mark
         </p>
       </header>
     </div>
