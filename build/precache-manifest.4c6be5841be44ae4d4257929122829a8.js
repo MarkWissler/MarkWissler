7aa68daf95f9b1f38831af6e48334c94f9205d2c
Mark Wissler <mark.wissler@exaptive.com>
2020-05-08 16:17:37 -0500

    oops

---
+++ build/precache-manifest.4c6be5841be44ae4d4257929122829a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c899abe40715c11065812f2fb0afb14f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1bc0a7d4571c33e8ff3",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bb1816c5e03fca7164c5",
+    "url": "/static/js/2.3d54e5fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3d54e5fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1bc0a7d4571c33e8ff3",
+    "url": "/static/js/main.1828b5f7.chunk.js"
+  },
+  {
+    "revision": "30d018af487bcf6bd9cf",
+    "url": "/static/js/runtime-main.338b5bd7.js"
+  }
+]);
